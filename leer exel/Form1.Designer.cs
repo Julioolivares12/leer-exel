69c5f22468228192de9522acb6b9340e87cac799
julio <julioolivares90@gmail.com>
2019-02-01 14:09:14 -0600

    Agregar archivos de proyecto.

---
+++ leer exel/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace leer_exel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnabrir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblhoja = new System.Windows.Forms.Label();
+            this.lblfila = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnleer = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnabrir
+            // 
+            this.btnabrir.Location = new System.Drawing.Point(32, 32);
+            this.btnabrir.Name = "btnabrir";
+            this.btnabrir.Size = new System.Drawing.Size(75, 23);
+            this.btnabrir.TabIndex = 0;
+            this.btnabrir.Text = "abrir exel";
+            this.btnabrir.UseVisualStyleBackColor = true;
+            this.btnabrir.Click += new System.EventHandler(this.btnabrir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(207, 38);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(46, 17);
+            this.lblnombre.TabIndex = 1;
+            this.lblnombre.Text = "label1";
+            // 
+            // lblhoja
+            // 
+            this.lblhoja.AutoSize = true;
+            this.lblhoja.Location = new System.Drawing.Point(32, 94);
+            this.lblhoja.Name = "lblhoja";
+            this.lblhoja.Size = new System.Drawing.Size(35, 17);
+            this.lblhoja.TabIndex = 2;
+            this.lblhoja.Text = "hoja";
+            // 
+            // lblfila
+            // 
+            this.lblfila.AutoSize = true;
+            this.lblfila.Location = new System.Drawing.Point(32, 149);
+            this.lblfila.Name = "lblfila";
+            this.lblfila.Size = new System.Drawing.Size(26, 17);
+            this.lblfila.TabIndex = 3;
+            this.lblfila.Text = "fila";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "columna";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // btnleer
+            // 
+            this.btnleer.Location = new System.Drawing.Point(79, 287);
+            this.btnleer.Name = "btnleer";
+            this.btnleer.Size = new System.Drawing.Size(75, 23);
+            this.btnleer.TabIndex = 9;
+            this.btnleer.Text = "leer";
+            this.btnleer.UseVisualStyleBackColor = true;
+            this.btnleer.Click += new System.EventHandler(this.btnleer_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(443, 148);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 325);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.btnleer);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblfila);
+            this.Controls.Add(this.lblhoja);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.btnabrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnabrir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lblhoja;
+        private System.Windows.Forms.Label lblfila;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnleer;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ leer exel/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leer_exel
+{
+    public partial class Form1 : Form
+    {
+
+        Microsoft.Office.Interop.Excel.Application _exelapplication;
+        Workbook workbook;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnabrir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                string name = openFileDialog1.FileName;//se guarda el nombre la ruta del archivo
+                _exelapplication = new Microsoft.Office.Interop.Excel.Application(); //creo un objeto de titpo aplicacion
+                workbook = _exelapplication.Workbooks.Open(name); //un libro de exel
+                lblnombre.Text = name;
+                //MessageBox.Show("OK !");
+            }else if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("cancel !");
+            }
+        }
+
+        private void btnleer_Click(object sender, EventArgs e)
+        {
+            int hoja = Convert.ToInt32(textBox1.Text);
+            int fila = Convert.ToInt32(textBox2.Text);
+            int col = Convert.ToInt32(textBox3.Text);
+
+            Worksheet worksheet = (Worksheet)workbook.Sheets[hoja];
+
+            string value = (((Range)worksheet.Cells[fila,col]).Value2).ToString();
+
+            textBox4.Text = value;
+        }
+    }
+}
